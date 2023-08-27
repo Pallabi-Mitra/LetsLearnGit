8c51e8bcc3245a8dc728c312be8050a857029568
Pallabi <pallabimitra1998@gmail.com>
2023-08-27 21:00:23 +0530

    Added new feature

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+public class Multiply {
+
+    public static void main()
+    {
+        int a= 10;
+        int b = 20;
+
+        int multiply = a*b;
+
+        System.out.println("The multiplication is : "+multiply);
+    }
+    
+}
